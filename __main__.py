7ee94b1ddd88686eb8e10187dfb7e34e9f9f55d8
Sam Bruno <sjbrunster@gmail.com>
2017-04-21 17:32:33 -0400

    Add pyqt5 gui and connect to analysis

--- __main__.py
+++ __main__.py
@@ -1,49 +1,74 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
 import sys
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import *
+from core.data.test_station import *
+from core.plotting.plots import *
+from core.histograms.histograms import *
+from core.limits.limits_parser import *
+from core.analysis.analysis import *
 
-class Example(QWidget):
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIcon
+from random import randint
+
+
+class TestAnalysisUI(QWidget):
     
     def __init__(self):
         super().__init__()
+        self.test_name = ''
         self.data_folder = ''
         self.limits_file = ''
         self.temp_buttons = []
         self.board_buttons = []
+        self.analysis_buttons = []
         self.init_ui()
 
-    def init_ui(self):      
+    def init_ui(self):
         grid = QGridLayout()
         self.setLayout(grid)
         grid.setSpacing(10)
 
-        order = [self.data_folder, self.limits_file, self.temp_buttons, self.board_buttons]
+        order = [self.data_folder, self.limits_file, self.temp_buttons, self.board_buttons] # dynamic row order?
 
-        self.data_folder_label = QLabel('(No folder selected)', self)
-        self.data_folder_button = FolderButton('Select Data Folder', self.data_folder_label, self)
+        ## data folder
+        self.data_folder_text_box = QLineEdit('(No Folder Selected)', self)
+        self.data_folder_button = FolderButton('Select Data Folder', self.data_folder_text_box, self)
         grid.addWidget(self.data_folder_button, 0, 0)
-        grid.addWidget(self.data_folder_label, 0, 1)
+        grid.addWidget(self.data_folder_text_box, 0, 1, 1, 6)
 
-        self.limits_label = QLabel('(No folder selected)', self)
-        self.limits_button = FolderButton('Select Data Folder', self.data_folder_label, self)
-        grid.addWidget(self.data_folder_button, 0, 0)
-        grid.addWidget(self.data_folder_label, 0, 1)
+        ## limits
+        self.limits_text_box = QLineEdit('(Leave blank if no limits)', self)
+        self.limits_button = LimitsButton('Select Limits File', self.limits_text_box, self)
+        grid.addWidget(self.limits_button, 1, 0)
+        grid.addWidget(self.limits_text_box, 1, 1, 1, 6)
 
-        self.temp_buttons = self.populate_buttons(DataButton, ['-40C', '23C', '85C'], 1, grid)
-        self.board_buttons = self.populate_buttons(DataButton, ['B1','B2','B3','B4','B5','B6'], 2, grid) 
+        ## temperature/board/analysis buttons
+        self.temp_buttons = self.populate_buttons('Temperatures:', TempButton, ['-40C', '23C', '45C', '60C', '85C'], 2, grid)
+        self.board_buttons = self.populate_buttons('Boards:', DataButton, ['B1','B2','B3','B4','B5','B6'], 3, grid) 
+        self.analysis_buttons = self.populate_buttons('Analysis:', DataButton, ['Plot', 'Histograms', 'Tables'], 4, grid)
 
+        ## test name
+        grid.addWidget(QLabel('Test Name:'), 5, 0)
+        self.test_name = QLineEdit('', self)
+        grid.addWidget(self.test_name, 5, 1, 1, 6)
+
+        ## analyze button
+        self.analyze_button = AnalyzeButton('Analyze', self)
+        grid.addWidget(self.analyze_button, 6, 2, 1, 3)
+
+        ## gui window properties
         self.move(300, 150) # center window
-        self.setWindowTitle('Testing...')
+        self.setWindowTitle('Automotive Testing Data Analysis')
+        self.setWindowIcon(QIcon('images\car.png'))
         self.show()
 
-
-    def populate_buttons(self, button_type, text_list, row, grid):
+    def populate_buttons(self, label, button_type, text_list, row, grid):
+        grid.addWidget(QLabel(label), row, 0)
         button_list = []
-        positions = [(row, j) for j in range(len(text_list))]
+        positions = [(row, j+1) for j in range(len(text_list))]
         for position, text in zip(positions, text_list):
             button = button_type(text, self)
             grid.addWidget(button, *position)
@@ -51,8 +76,40 @@ class Example(QWidget):
         return button_list
 
     def get_row(self, widget):
-        ''' Idea: get row for passed widget or widget group '''
+        ''' Idea (not implemented): get row for passed widget or widget group '''
         pass
+
+
+class FolderButton(QPushButton):
+
+    def __init__(self, text, text_box, ui):
+        super().__init__()
+        self.ui = ui
+        self.setText(text)
+        self.text_box = text_box
+        self.name = ''
+        self.clicked.connect(self.set_folder)
+
+    def set_folder(self):
+        self.name = str(QFileDialog.getExistingDirectory(self, "Select directory for data analysis"))
+        self.text_box.setText(self.name)
+        self.ui.data_folder = self.name
+
+
+class LimitsButton(QPushButton):
+
+    def __init__(self, text, text_box, ui):
+        super().__init__()
+        self.ui = ui
+        self.setText(text)
+        self.text_box = text_box
+        self.name = ''
+        self.clicked.connect(self.set_limits)
+
+    def set_limits(self):
+        self.name = str(QFileDialog.getOpenFileName(self, "Select limits file to use for data analysis")[0])
+        self.text_box.setText(self.name)
+        self.ui.limits_file = self.name
 
 
 class DataButton(QPushButton):
@@ -76,40 +133,68 @@ class DataButton(QPushButton):
             self.setStyleSheet('background-color: None; color: None;')
 
 
-class FolderButton(QPushButton):
+class TempButton(DataButton):
 
-    def __init__(self, text, label, ui):
+    def __init__(self, name, ui):
+        super().__init__(name, ui)
+        self.init_temp()
+
+    def init_temp(self):
+        self.temp = int(self.name[:-1])
+
+
+class AnalyzeButton(QPushButton):
+    
+    def __init__(self, name, ui):
         super().__init__()
-        self.setText(text)
-        self.label = label
-        self.name = ''
-        self.clicked.connect(self.pressed)
+        self.init_button(name)
+        self.ui = ui
 
-    def pressed(self):
-        self.name = str(QFileDialog.getExistingDirectory(self, "Select Directory for Data Analysis"))
-        self.label.setText(self.name)
+    def init_button(self, name):
+        self.setText(name)
+        self.name = name
+        self.clicked[bool].connect(self.analyze)
 
+    def analyze(self):
+        test_name = self.ui.test_name.text()
+        temps = [t.temp for t in self.ui.temp_buttons if t.pressed]
+        boards = [b.name for b in self.ui.board_buttons if b.pressed]
+        datapath = self.ui.data_folder
+        limits = self.load_limits()     
+       
+        if boards and temps and datapath:
+            self.print_test_conditions(test_name, temps, boards)
+            test = TestStation(boards, datapath, limits, *temps)
+            for analysis_type in self.ui.analysis_buttons:
+                if analysis_type.pressed:
+                    self.run_analysis(analysis_type.name, test, limits)
+        else:
+            print('You must select a data folder, temperatures, and test boards')
 
-class LimitsButton(QPushButton):
+    def print_test_conditions(self, test_name, temps, boards):
+        print('\nTest Name:', test_name)
+        print('Data Folder:', self.ui.data_folder)
+        print('Limits File:', self.ui.limits_file)
+        print('Temperatures:', temps)
+        print('Boards:', boards, '\n')
 
-''' do this instead:
-http://stackoverflow.com/questions/31728253/pyqt-open-file-dialog-display-path-name
-'''
+    def run_analysis(self, analysis_name, test, limits):
+        if analysis_name == 'Plot':
+            plot_modes(test)
+        elif analysis_name == 'Histograms':
+            make_mode_histograms(test, system_by_system=False, limits=limits)
+        elif analysis_name == 'Tables':
+            fill_stats(test, limits)
 
-    def __init__(self, text, label, ui):
-        super().__init__()
-        self.setText(text)
-        self.label = label
-        self.name = ''
-        self.clicked.connect(self.pressed)
-
-    def pressed(self):
-        self.name = str(QFileDialog.getExistingDirectory(self, "Select Directory for Data Analysis"))
-        self.label.setText(self.name)
+    def load_limits(self):
+        if self.ui.limits_file:
+            return Limits(self.ui.limits_file, 'Sheet1')
+        else:
+            return None
 
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-    ex = Example()
+    gui = TestAnalysisUI()
     sys.exit(app.exec_())
--- core/histograms/histograms.py
+++ core/histograms/histograms.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt, mpld3
 import matplotlib.dates as dates
+from core.data.helpers import *
 
 
 def make_mode_histograms(test, system_by_system=True, limits=None):
@@ -24,7 +25,7 @@ def histogram_of_each_system(test, mode, temp, limits=None):
         fig = plt.figure()
         bar_color = determine_bar_color(temp)
         if limits: ## if doing limit analysis
-            LL, UL = limits[temp][mode.mode_tag][voltage][0], limits[temp][mode.mode_tag][voltage][1]
+            LL, UL = limits.lim[temp][mode.mode_tag][voltage][0], limits.lim[temp][mode.mode_tag][voltage][1]
             title = ' '.join([mode.board_mode, str(temp), str(voltage), ' LL:', str(LL), ' UL:', str(UL)])
         else:
             title = ' '.join([mode.board_mode, str(temp), str(voltage)])
@@ -34,7 +35,7 @@ def histogram_of_each_system(test, mode, temp, limits=None):
         nrows, ncols = make_subplot_layout(num_subplots)
         i = 1
         for system in mode.systems:
-            filtered_df = mode.filter_temp_and_voltage(mode.df[[mode.AMB_TEMP, mode.VSETPOINT, system]], temp, voltage)
+            filtered_df = filter_temp_and_voltage(mode.df[[mode.AMB_TEMP, mode.VSETPOINT, system]], temp, voltage)
             current_data = pd.to_numeric(filtered_df[system], downcast='float')
             avg = current_data.mean()
             sigma = current_data.std()
@@ -55,18 +56,18 @@ def histogram_of_each_system(test, mode, temp, limits=None):
 
 def histogram_of_mode(test, mode, temp, limits=None):
     fig = plt.figure()
-    if limits: ## if doing limit analysis
-        LL, UL = limits[temp][mode.mode_tag][voltage][0], limits[temp][mode.mode_tag][voltage][1]
-        title = ' '.join([mode.board_mode, str(temp), ' LL:', str(LL), ' UL:', str(UL)])
-    else:
-        title = ' '.join([mode.board_mode, str(temp)])
-    fig.canvas.set_window_title(title)
-    fig.suptitle(title, fontsize = 14, fontweight='bold')
     nrows, ncols = len(mode.voltages), 1
+    main_title = ' '.join([mode.board_mode, str(temp)])
+    fig.canvas.set_window_title(main_title)
+    fig.suptitle(main_title, fontsize = 14, fontweight='bold')
 
     i = 1
     for voltage in mode.voltages: # make subplot for each voltage
-        #current_data = mode.hist_dict[temp][voltage]
+        if limits: ## if doing limit analysis
+            LL, UL = get_limits_at_mode_temp_voltage(limits, mode, temp, voltage)
+            subtitle = ' '.join([str(voltage)+'V', ' LL:', str(LL), ' UL:', str(UL)])
+        else:
+            subtitle = str(voltage)+'V' 
         dframe = mode.hist_dict[temp][voltage]
         current_data = mode.strip_index_and_melt_to_series(dframe)
         avg = current_data.mean()
@@ -83,7 +84,7 @@ def histogram_of_mode(test, mode, temp, limits=None):
         else:
             ax.axvline(avg-3*sigma, color='b', linestyle='dotted', linewidth=2)
             ax.axvline(avg+3*sigma, color='b', linestyle='dotted', linewidth=2)
-        ax.set_title(str(voltage) + 'V')
+        ax.set_title(subtitle)
         ax.set_xlabel('Current (A)', fontsize=8)
         ax.set_ylabel('Frequency', fontsize=8)
         plt.setp(ax.get_xticklabels(), fontsize=8)
@@ -92,7 +93,8 @@ def histogram_of_mode(test, mode, temp, limits=None):
 
     plt.tight_layout()
     plt.subplots_adjust(top=0.87, bottom=0.05, left=0.07, right=0.97)
-  
+
+
 def make_subplot_layout(num):
     nrows, ncols = 0, 0
     if num > 10:
