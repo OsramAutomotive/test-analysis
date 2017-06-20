cf2eb6671a51dd79529d0fef54686fee8662460f
Sam Bruno <sjbrunster@gmail.com>
2017-06-20 16:43:54 -0400

    implement sys by sys hists for led binnign

--- core/analysis/histograms.py
+++ core/analysis/histograms.py
@@ -11,7 +11,7 @@ from core.data_import.helpers import *
 def make_mode_histograms(test, system_by_system=True, limits=None):
     print('Plotting histograms...\n')
     for mode in test.modes:
-        for temp in mode.temps:         
+        for temp in mode.temps:
             if system_by_system:
                 if temp in mode.hist_dict:
                     histogram_of_each_system(test, mode, temp, limits)
@@ -22,15 +22,19 @@ def make_mode_histograms(test, system_by_system=True, limits=None):
     plt.show()
 
 def histogram_of_each_system(test, mode, temp, limits=None):
-    ### TO DO ---> implement LED binning for system-by-system
-
     for voltage in mode.voltages: ## make plot for mode in each voltage
         num_subplots = len(mode.systems)
         fig = plt.figure()
         bar_color = determine_bar_color(temp)
+        title = ' '.join([test.name+'\n', mode.name, str(temp), str(voltage)])
         if limits and test.run_limit_analysis: ## if doing limit analysis
-            LL, UL = limits.lim[temp][mode.mode_tag][voltage][0], limits.lim[temp][mode.mode_tag][voltage][1]
-            title = ' '.join([mode.name, str(temp), str(voltage), ' LL:', str(LL), ' UL:', str(UL)])
+            mode_limits_dict = get_limits_at_mode_temp_voltage(limits, mode, temp, voltage)
+            if mode.has_led_binning:
+                for lim_label, lim_value in sorted(mode_limits_dict.items()):
+                    title += '  ' + lim_label + ': ' + str(lim_value)
+            else:
+                LL, UL = mode_limits_dict['LL'], mode_limits_dict['UL']
+                title += '  LL: ' + str(LL) + '  UL: ' + str(UL)
         else:
             title = ' '.join([mode.name, str(temp), str(voltage)])
         fig.canvas.set_window_title(title)
@@ -44,20 +48,24 @@ def histogram_of_each_system(test, mode, temp, limits=None):
             avg = current_data.mean()
             sigma = current_data.std()
             ax = fig.add_subplot(nrows, ncols, i)
-            ax.set_title(test.systems[i-1])
             ax.hist(current_data.dropna(), color=bar_color)  ## drop NaN values
             ax.axvline(avg, color='dimgray', linestyle='dotted', linewidth=4)
-            if limits and test.run_limit_analysis:
-                ax.axvline(LL, color='red', linestyle='dashed', linewidth=2)
-                ax.axvline(UL, color='red', linestyle='dashed', linewidth=2)
+
+            if limits and test.run_limit_analysis:  ## show current limits
+                if mode.has_led_binning:
+                    led_bin = get_system_bin(mode, system)
+                    mode_limits_dict = get_limits_for_system_with_binning(limits, mode, temp, voltage, system)
+                    LL, UL = mode_limits_dict['LL'], mode_limits_dict['UL']
+                ax.axvline(LL, color='orangered', linestyle='dashed', linewidth=2)
+                ax.axvline(UL, color='orangered', linestyle='dashed', linewidth=2)
             else:
                 ax.axvline(avg-3*sigma, color='b', linestyle='dashed', linewidth=2)
                 ax.axvline(avg+3*sigma, color='b', linestyle='dashed', linewidth=2)
+            ax.set_title(test.systems[i-1])
             ax.set_xlabel('Current (A)')
             ax.set_ylabel('Frequency')
             ax.get_xaxis().get_major_formatter().set_useOffset(False)
             i += 1
-
 
 def histogram_of_mode(test, mode, temp, limits=None):
     if mode.has_led_binning:
@@ -71,13 +79,12 @@ def histogram_of_mode_with_binning(test, mode, temp, limits, led_bin):
 
     fig = plt.figure()
     nrows, ncols = len(mode.voltages), 1
-    main_title = ' '.join([test.name, mode.name, str(temp), ' LED bin', led_bin])
+    main_title = ' '.join([test.name+'\n', mode.name, str(temp)+u'\N{DEGREE SIGN}'+'C', ' LED bin', led_bin])
     fig.canvas.set_window_title(main_title)
     fig.suptitle(main_title, fontsize = 14, fontweight='bold')
 
     i = 1
     for voltage in mode.voltages: # make subplot for each voltage
-        subtitle = str(voltage)+'V'
         dframe = mode.hist_dict[temp][voltage]
         current_data = mode.strip_index_and_melt_to_series_for_binning(dframe, led_bin) ## put all system currents in single series
         avg = current_data.mean()
@@ -85,6 +92,7 @@ def histogram_of_mode_with_binning(test, mode, temp, limits, led_bin):
         minus_ten = round(avg*0.9, 3)
         plus_ten = round(avg*1.1, 3)
 
+        subtitle = str(voltage)+'V'+'  Avg: '+str(round(avg, 3))
         ax = fig.add_subplot(nrows, ncols, i)
         ax.hist(current_data.dropna(), color='dimgray')  ## drop NaN values
         ax.axvline(avg, color='k', linestyle='dotted', linewidth=2)
@@ -108,23 +116,20 @@ def histogram_of_mode_with_binning(test, mode, temp, limits, led_bin):
         plt.setp(ax.get_xticklabels(), fontsize=8)
         plt.setp(ax.get_yticklabels(), fontsize=8)
         i += 1
-
     plt.tight_layout()
     plt.subplots_adjust(top=0.87, bottom=0.05, left=0.07, right=0.97)
-
 
 
 def histogram_of_mode_no_binning(test, mode, temp, limits=None):
 
     fig = plt.figure()
     nrows, ncols = len(mode.voltages), 1
-    main_title = ' '.join([test.name, mode.name, str(temp)])
+    main_title = ' '.join([test.name+'\n', mode.name, str(temp)+u'\N{DEGREE SIGN}'+'C'])
     fig.canvas.set_window_title(main_title)
     fig.suptitle(main_title, fontsize = 14, fontweight='bold')
 
     i = 1
     for voltage in mode.voltages: # make subplot for each voltage
-        subtitle = str(voltage)+'V'
         dframe = mode.hist_dict[temp][voltage]
         current_data = mode.strip_index_and_melt_to_series(dframe) ## put all system currents in single series
         avg = current_data.mean()
@@ -132,6 +137,7 @@ def histogram_of_mode_no_binning(test, mode, temp, limits=None):
         minus_ten = round(avg*0.9, 3)
         plus_ten = round(avg*1.1, 3)
 
+        subtitle = str(voltage)+'V'+'  Avg: '+str(round(avg, 3))
         ax = fig.add_subplot(nrows, ncols, i)
         ax.hist(current_data.dropna(), color='dimgray')  ## drop NaN values
         ax.axvline(avg, color='k', linestyle='dotted', linewidth=2)
